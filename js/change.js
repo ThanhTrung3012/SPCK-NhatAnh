71def6154e5c2f4c09438804f2115bb661694e90
ThanhTrung3012 <thanhtrungdo30120@gmail.com>
2025-07-13 11:59:13 +0700

    update4

--- js/change.js
+++ js/change.js
@@ -1,13 +1,13 @@
-const container = document.getElementById("container");
-const toSignIn = document.getElementById("switch-to-signin");
-const toSignUp = document.getElementById("switch-to-signup");
+// const container = document.getElementById("container");
+// const toSignIn = document.getElementById("switch-to-signin");
+// const toSignUp = document.getElementById("switch-to-signup");
 
-toSignIn?.addEventListener("click", function (e) {
-  e.preventDefault();
-  container.classList.remove("active");
-});
+// toSignIn?.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   container.classList.remove("active");
+// });
 
-toSignUp?.addEventListener("click", function (e) {
-  e.preventDefault();
-  container.classList.add("active");
-});
+// toSignUp?.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   container.classList.add("active");
+// });
--- js/firebase.js
+++ js/firebase.js
@@ -41,7 +41,7 @@ if (loginGG) {
 
     if (user) {
       // Nếu đang ở trang login mà đã đăng nhập → chuyển sang home
-      if (currentPath === "index.html") {
+      if (currentPath === "/index.html") {
         window.location.href = "/html/home.html";
       } else {
         // Đã đăng nhập, hiển thị thông tin người dùng
@@ -77,8 +77,8 @@ if (loginGG) {
       }
     } else {
       // Nếu đang KHÔNG ở login.html → redirect về login
-      if (currentPath !== "index.html") {
-        window.location.href = "index.html";
+      if (currentPath !== "/index.html") {
+        window.location.href = "/index.html";
       }
     }
   });
@@ -87,7 +87,7 @@ if (loginGG) {
     logoutBtn.addEventListener("click", function () {
       signOut(auth)
         .then(() => {
-          window.location.href = "index.html";
+          window.location.href = "/index.html";
         })
         .catch((error) => {
           console.log(error);
